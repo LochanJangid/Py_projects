64220c7b446576ad6fff6dc052b672637a87b5d2
lochan jangid <lochanjangidcoder@gmail.com>
2026-04-14 21:21:21 +0000

    delete online bank project

--- phonebook (WEB APP)/app.py
+++ phonebook (WEB APP)/app.py
@@ -1,8 +1,7 @@
-import sqlite3
+import sqliter
 import re
 from flask import Flask, render_template, session, request, redirect, jsonify
 from datetime import timedelta
-from database import Database
 from flask_bcrypt import Bcrypt
 from jinja2.exceptions import TemplateNotFound
 app = Flask(__name__)
@@ -12,7 +11,7 @@ app.secret_key = 'hhhhhhhhh'
 app.permanent_session_lifetime = timedelta(days=30)
 # session.permanent = True
 
-db = Database('phonebook.db')
+db = sqliter.Database('phonebook.db')
 
 @app.errorhandler(TemplateNotFound)
 def handle_template_not_found(e):
@@ -20,7 +19,7 @@ def handle_template_not_found(e):
 
 @app.route('/')
 def index():
-    contacts = db.query('SELECT * FROM phonebook', "fetchall")
+    contacts = db.query('SELECT * FROM phonebook', operation="fetchall")
     if not session.get("username"):
         return render_template('home.html')
     return render_template('index.html', contacts=contacts, username=session["username"])
@@ -39,7 +38,7 @@ def register():
         if not username and not password and not confirm_password:
             errors.append('PLEASE FILL ALL DETAILS!')
 
-        registerd_username = db.query('SELECT * FROM users WHERE username = ?', username, operation="fetchall")
+        registerd_username = db.query('SELECT * FROM users WHERE username = ?', (username, ), operation="fetchall")
         if registerd_username:
             errors.append('USERNAME IS ALREADY EXIST!')
 
@@ -58,7 +57,7 @@ def register():
             return jsonify({'errors': errors})
 
         hash = bcrypt.generate_password_hash(password).decode('utf-8')
-        db.query('INSERT INTO users (username, email, password) VALUES (?, ?, ?)', username, email, hash)
+        db.query('INSERT INTO users (username, email, password) VALUES (?, ?, ?)', (username, email, hash))
         session["username"] = username
 
         return redirect('/')
@@ -77,7 +76,7 @@ def login():
         if not username or not password:
             errors.append('PLEASE FILL USERNAME AND PASSWORD')
 
-        user_id = db.query('SELECT * FROM users WHERE username = ?', username, operation="fetchone")
+        user_id = db.query('SELECT * FROM users WHERE username = ?', (username, ), operation="fetchone")
 
         if user_id is None:
             errors.append('USERNAME DOES NOT EXIST!')
@@ -126,10 +125,10 @@ def add_contact():
             return render_template('add_contact.html', errors=errors)  
 
         # insert new contact in phonebook
-        user_id = db.query('SELECT * FROM users WHERE username = ?', session["username"], operation="fetchone")
+        user_id = db.query('SELECT * FROM users WHERE username = ?', (session["username"], ), operation="fetchone")
         if user_id is None:
             return 'INVALID USER'
-        db.query('INSERT INTO phonebook (name, number, email, relation, company, user_id) VALUES(?, ?, ?, ?, ?, ?)', name, number, email, relation, company, user_id["id"])
+        db.query('INSERT INTO phonebook (name, number, email, relation, company, user_id) VALUES(?, ?, ?, ?, ?, ?)',( name, number, email, relation, company, user_id["id"]))
         return redirect('/')
 
     return render_template('add_contact.html')
@@ -139,7 +138,7 @@ def contact_detail():
     if not session.get("username"):
         return render_template('/')
     id = request.form.get("id")
-    details = db.query('SELECT * FROM phonebook WHERE id = ?', id, operation="fetchone")
+    details = db.query('SELECT * FROM phonebook WHERE id = ?', (id, ), operation="fetchone")
     return render_template('contact_detail.html', details=details)
 
 @app.route('/delete_contact', methods=["POST"])
@@ -147,7 +146,7 @@ def delete_contact():
     if not session.get("username"):
         return render_template('/')    
     id = request.form.get("id")
-    db.query('DELETE FROM phonebook WHERE id = ?', id)
+    db.query('DELETE FROM phonebook WHERE id = ?', (id, ))
     return redirect('/')
 
 @app.route('/show_contacts')
@@ -158,7 +157,7 @@ def show_contacts():
     if request.args.get("q"):    
         q = request.args.get("q")
     
-    contacts = db.query('SELECT * FROM phonebook WHERE name LIKE ? AND user_id = (SELECT id FROM users WHERE username = ?)', '%'+q+'%', session["username"], operation="fetchall")
+    contacts = db.query('SELECT * FROM phonebook WHERE name LIKE ? AND user_id = (SELECT id FROM users WHERE username = ?)', ('%'+q+'%', session["username"]), operation="fetchall")
 
     return render_template('show_contacts.html', contacts=contacts)
 
@@ -196,5 +195,5 @@ def modify_contact():
         db.query('UPDATE phonebook SET name = ?, number =?,  email = ?,  relation = ?, company = ? WHERE id = ?', (name, number, email, relation, company, id))
         return redirect('/')
     
-    details = db.query('SELECT * FROM phonebook WHERE id = ?', request.args.get("id"), operation="fetchone")
+    details = db.query('SELECT * FROM phonebook WHERE id = ?', (request.args.get("id"), ), operation="fetchone")
     return render_template('modify_contact.html', details=details)  
